b9cfeb7173d1f05d70fc0244bbf9d2c7ac29180d
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-11 19:33:28 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args }) => {
     let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length;
     
     let txt = `
-Hola! Soy  *${botname}*  ٩(˘◡˘)۶
+Hola! Soy  *${botname}*  •(=^●ω●^=)•🌸
 Aquí tienes la lista de comandos
 ╭┈ ↷
 │ᰔᩚ Cliente » @${userId.split('@')[0]}
