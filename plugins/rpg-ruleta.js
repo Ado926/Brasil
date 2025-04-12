f8dbc28685c718087c81b956e5e1ca2957c7eb69
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-12 02:34:31 -0400

    Actualizar rpg-ruleta.js

--- plugins/rpg-ruleta.js
+++ plugins/rpg-ruleta.js
@@ -7,29 +7,29 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
 
   if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempoEspera * 1000) {
     let tiempoRestante = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempoEspera * 1000 - Date.now()) / 1000))
-    conn.reply(m.chat, `${emoji3} Ya has iniciado una apuesta recientemente, espera *⏱ ${tiempoRestante}* para apostar nuevamente`, m)
+    conn.reply(m.chat, `《✧》 Ya has iniciado una apuesta recientemente, espera *⏱ ${tiempoRestante}* para apostar nuevamente`, m)
     return
   }
 
   cooldowns[m.sender] = Date.now()
 
-  if (!text) return conn.reply(m.chat, `${emoji} Debes ingresar una cantidad de *⛀ ${moneda}* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m)
+  if (!text) return conn.reply(m.chat, `《✧》 Debes ingresar una cantidad de *⛀ ${moneda}* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m)
 
   let args = text.trim().split(" ")
-  if (args.length !== 2) return conn.reply(m.chat, `${emoji2} Formato incorrecto. Debes ingresar una cantidad de *⛀ ${moneda}* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m)
+  if (args.length !== 2) return conn.reply(m.chat, `《✧》 Formato incorrecto. Debes ingresar una cantidad de *⛀ ${moneda}* y apostar a un color, por ejemplo: *${usedPrefix + command} 20 black*`, m)
 
   let coin = parseInt(args[0])
   let color = args[1].toLowerCase()
 
-  if (isNaN(coin) || coin <= 0) return conn.reply(m.chat, `${emoji} Por favor, ingresa una cantidad válida para la apuesta.`, m)
+  if (isNaN(coin) || coin <= 0) return conn.reply(m.chat, `《✧》 Por favor, ingresa una cantidad válida para la apuesta.`, m)
 
-  if (coin > 10000) return conn.reply(m.chat, `${emoji}  La cantidad máxima de apuesta es de 50 ${moneda}.`, m)
+  if (coin > 10000) return conn.reply(m.chat, `✎  La cantidad máxima de apuesta es de 50 ${moneda}.`, m)
 
   if (!(color === 'black' || color === 'red')) return conn.reply(m.chat, `${emoji2} Debes apostar a un color válido: *black* o *red*.`, m)
 
-  if (coin > users.coin) return conn.reply(m.chat, `${emoji2} No tienes suficientes ${moneda} para realizar esa apuesta.`, m)
+  if (coin > users.coin) return conn.reply(m.chat, `《✧》 No tienes suficientes ${moneda} para realizar esa apuesta.`, m)
 
-  await conn.reply(m.chat, `${emoji} Apostaste ${coin} *⛀ ${moneda}* al color ${color}. Espera *⏱ 10 segundos* para conocer el resultado.`, m)
+  await conn.reply(m.chat, `❀ Apostaste ${coin} *⛀ ${moneda}* al color ${color}. Espera *⏱ 10 segundos* para conocer el resultado.`, m)
 
   setTimeout(() => {
     let result = Math.random()
@@ -43,10 +43,10 @@ let handler = async (m, { conn, text, command, usedPrefix }) => {
 
     if (win) {
       users.coin += coin
-      conn.reply(m.chat, `${emoji} ¡Ganaste! Obtuviste ${coin} *⛀ ${moneda}*. Total: ${users.coin} *⛀ ${moneda}*.`, m)
+      conn.reply(m.chat, `❀ ¡Ganaste! Obtuviste ${coin} *⛀ ${moneda}*. Total: ${users.coin} *⛀ ${moneda}*.`, m)
     } else {
       users.coin -= coin
-      conn.reply(m.chat, `${emoji2} Perdiste. Se restaron ${coin} *⛀ ${moneda}*. Total: ${users.coin} *⛀ ${moneda}*.`, m)
+      conn.reply(m.chat, `❀ Perdiste. Se restaron ${coin} *⛀ ${moneda}*. Total: ${users.coin} *⛀ ${moneda}*.`, m)
     }
 
 
