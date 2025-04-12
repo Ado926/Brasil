ccae44b9ae622e5340e16c3c701f3a756bea139c
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-11 23:57:01 -0400

    Actualizar main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -102,7 +102,7 @@ var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
 
 global.packsticker = `❀sumichannel❀\n Bot:${botname}\n👑 Usuario: ${nombre}\n✎Fecha: ${fecha}\nⴵ Hora: ${tiempo}`;
-global.packsticker2 = `\n°leo\n\n${dev}`
+global.packsticker2 = `\n︵۪ׄ┅̤ׄ─⃔͜┉࣪━〫֮🎀̶̤┉⃨࣪━〫֮─⃕͜°\n\n${dev}`
   
 //Fakes
 global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `${nombre}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${nombre},;;;\nFN:${nombre},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': null, thumbnail: null,sendEphemeral: true}}}
