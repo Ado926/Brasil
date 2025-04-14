18a0aca24fc6f7c1b3c26753eaad7f873f130cb3
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-13 22:09:21 -0400

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -1,9 +1,9 @@
-//kimport moment from 'moment-timezone'
-//import fetch from 'node-fetch'
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
 try {
-let res = await fetch('https://api.github.com/repos/The-King-Destroy/Yuki_Suou-Bot')
+let res = await fetch('https://github.com/leoneloficial/Brasil')
 
 if (!res.ok) throw new Error('Error al obtener datos del repositorio')
 let json = await res.json()
@@ -18,7 +18,7 @@ txt += `✩  *Forks* : ${json.forks_count}\n`
 txt += `✩  *Stars* : ${json.stargazers_count}\n\n`
 txt += `> *${dev}*`
 
-await conn.sendMessage(m.chat, {text: txt, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterName: channelRD.name, newsletterJid: channelRD.id, }, externalAdReply: { title: packname, body: dev, thumbnailUrl: 'https://qu.ax/nGaLj.jpg', sourceUrl: redes, mediaType: 1, renderLargerThumbnail: true }}}, {quoted: fkontak})
+await conn.sendMessage(m.chat, {text: txt, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterName: channelRD.name, newsletterJid: channelRD.id, }, externalAdReply: { title: packname, body: dev, thumbnailUrl: 'https://files.catbox.moe/m98als.jpg', sourceUrl: redes, mediaType: 1, renderLargerThumbnail: true }}}, {quoted: fkontak})
 
 } catch {
 await conn.reply(m.chat, `${msm} Ocurrió un error.`, m)
