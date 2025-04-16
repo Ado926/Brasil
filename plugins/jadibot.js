7873fd238c9384bb6185b794838b681354dfc102
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-04-16 11:16:21 -0400

    Actualizar jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -67,7 +67,7 @@ resultado += segundos + " segundos";
 }
 return resultado;
 }
-const message = users.map((v, index) => `• 「 ${index + 1} 」\n「❀」 Wa.me/${v.user.jid.replace(/[^0-9]/g, '')}\n❀ Usuario: ${v.user.name || 'Sub-Bot'}\nⴵ Online: ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : 'Desconocido'}`).join('\n\n__________________________\n\n');
+const message = users.map((v, index) => `❀⃝⵿ᩙ ✿ꨩ꯭੭ 「 ${index + 1} 」\n「❀」 Wa.me/${v.user.jid.replace(/[^0-9]/g, '')}\n❀ Usuario: ${v.user.name || 'Sub-Bot'}\nⴵ Online: ${ v.uptime ? convertirMsADiasHorasMinutosSegundos(Date.now() - v.uptime) : 'Desconocido'}`).join('\n\n__________________________\n\n');
 const replyMessage = message.length === 0 ? `No hay Sub-Bots disponible por el momento, verifique mas tarde.` : message;
 const totalUsers = users.length;
 const responseMessage = `${emoji} LISTA DE *SUB-BOTS* ACTIVOS\n\n${emoji2} PUEDES PEDIR PERMISO PARA QUE TE DEJEN UNIR EL BOT A TÚ GRUPO\n\n\`\`\`CADA USUARIO SUB-BOT USA SUS FUNCIONES COMO QUIERA, EL NÚMERO PRINCIPAL NO SE HACE RESPONSABLE DEL USO DEL MAL USO DE ELLA \`\`\`\n\n*SUB-BOT CONECTADOS:* ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
